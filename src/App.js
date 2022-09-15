72cd944253d72c0ae94134a71289b785ec2e509f
Patt0lino <investmentmoises@gmail.com>
2022-09-15 02:10:17 -0300

    criando estrutura base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+const [tasks, setTasks] =  useState([{
+  "name":"Lavar roupa",
+  "description":"nao esquecer da agua",
+},{
+  "name":"Limpar a cama",
+  "description":"Limpar travesseiro",
+},{
+  "name":"Matheus",
+  "description":"vindo das aguas",
+}
+])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="form">
+      <form action="/">
+          <input type="text" placeholder='Tarefa' />
+          <input type="text" placeholder='Descrição' />
+          <button type="submit" className='add'>Add. Tarefa</button>
+      </form>
+      </div>
+
+      <div className='tasks'>
+      {tasks.map((task)=>(
+              <div className='card-task'>
+                <h2>{task.name}</h2>
+                <p>{task.description}</p>
+                <button className='red'>Delete</button>
+              </div>
+            ))}
+      </div>
+
+
     </div>
   );
 }
 
 export default App;
+
+
+
+//objeto para guardar as tasks  
+//estrutura => nome, desc. e btn de excluir
+
+//componente de exibicao, criar um map e o componente dentro
+
+//
